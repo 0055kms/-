9ac9163a9b34b251b5340c117f87d8254dd6c9f1
Myungseok Kang <157768680+0055kms@users.noreply.github.com>
2024-07-13 12:28:23 +0900

    Ǳ

---
+++ Ǳ.js
@@ -0,0 +1,143 @@
+function printRepeatedNumbers(repetitions, count) {
+    for (let i = 0; i < repetitions; i++) {
+        for (let j = 1; j <= count; j++) {
+            console.log(j);
+        }
+    }
+}
+
+function main() {
+    printRepeatedNumbers(1000, 100);
+
+    for (let i = 0; i < 1000; i++) {
+        printRepeatedNumbers(1000, 100);
+    }
+
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+            printRepeatedNumbers(1000, 100);
+        }
+    }
+
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+            for (let k = 0; k < 1000; k++) {
+                printRepeatedNumbers(1000, 100);
+            }
+        }
+    }
+
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+            for (let k = 0; k < 1000; k++) {
+                for (let l = 0; l < 1000; l++) {
+                    printRepeatedNumbers(1000, 100);
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+            for (let k = 0; k < 1000; k++) {
+                for (let l = 0; l < 1000; l++) {
+                    for (let m = 0; m < 1000; m++) {
+                        printRepeatedNumbers(1000, 100);
+                    }
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+            for (let k = 0; k < 1000; k++) {
+                for (let l = 0; l < 1000; l++) {
+                    for (let m = 0; m < 1000; m++) {
+                        for (let n = 0; n < 1000; n++) {
+                            printRepeatedNumbers(1000, 100);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+            for (let k = 0; k < 1000; k++) {
+                for (let l = 0; l < 1000; l++) {
+                    for (let m = 0; m < 1000; m++) {
+                        for (let n = 0; n < 1000; n++) {
+                            for (let o = 0; o < 1000; o++) {
+                                printRepeatedNumbers(1000, 100);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+            for (let k = 0; k < 1000; k++) {
+                for (let l = 0; l < 1000; l++) {
+                    for (let m = 0; m < 1000; m++) {
+                        for (let n = 0; n < 1000; n++) {
+                            for (let o = 0; o < 1000; o++) {
+                                for (let p = 0; p < 1000; p++) {
+                                    printRepeatedNumbers(1000, 100);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+            for (let k = 0; k < 1000; k++) {
+                for (let l = 0; l < 1000; l++) {
+                    for (let m = 0; m < 1000; m++) {
+                        for (let n = 0; n < 1000; n++) {
+                            for (let o = 0; o < 1000; o++) {
+                                for (let p = 0; p < 1000; p++) {
+                                    for (let q = 0; q < 1000; q++) {
+                                        printRepeatedNumbers(1000, 100);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+            for (let k = 0; k < 1000; k++) {
+                for (let l = 0; l < 1000; l++) {
+                    for (let m = 0; m < 1000; m++) {
+                        for (let n = 0; n < 1000; n++) {
+                            for (let o = 0; o < 1000; o++) {
+                                for (let p = 0; p < 1000; p++) {
+                                    for (let q = 0; q < 1000; q++) {
+                                        for (let r = 0; r < 1000; r++) {
+                                            printRepeatedNumbers(1000, 100);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+main();
